2e8053d028a406d92d5d4548efe5de42cc4d73bb
Necati Evren Yaşar <evren.yasar@hacettepe.edu.tr>
2020-02-05 09:35:38 +0300

    User email persistent

--- TRPRestKit/Services/User/TRPUserRegister.swift
+++ TRPRestKit/Services/User/TRPUserRegister.swift
@@ -58,14 +58,11 @@ internal class TRPUserRegister: TRPRestServices {
     public override func parameters() -> [String: Any]? {
         
         if let email = email, let password = password {
-            
             var parameters = ["email": email,
-            "password": password ]
-            
+                              "password": password ]
             if let firstName = firstName {
                 parameters["first_name"] = firstName
             }
-            
             if let lastName = lastName {
                 parameters["last_name"] = lastName
             }
