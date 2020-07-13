9a741b3da5111236094812191da82fb117f12cda
Necati Evren Yaşar <evren.yasar@hacettepe.edu.tr>
2020-07-13 11:42:59 +0300

    Booking Model

--- TRPRestKit/Model/Place/TRPBookingModel.swift
+++ TRPRestKit/Model/Place/TRPBookingModel.swift
@@ -8,9 +8,11 @@
 
 import Foundation
 public struct TRPBookingInfoModel: Decodable {
+    
     var providerId: Int?
     var providerName: String?
     var product: TRPBookingProductInfoModel?
+    
     private enum CodingKeys: String, CodingKey {
         case providerId = "provider_id"
         case providerName = "provider_name"
@@ -28,8 +30,16 @@ public struct TRPBookingInfoModel: Decodable {
 }
 
 public struct TRPBookingProductInfoModel: Decodable {
-    var id: String
+    
+    var id: String?
     var title: String?
-    var transactions: [String]?
+    var additionalData: [String]?
     var url: String?
+    
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case title
+        case url
+        case additionalData = "additional_data"
+    }
 }
